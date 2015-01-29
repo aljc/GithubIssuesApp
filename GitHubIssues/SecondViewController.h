392088f4bfe007a3763ffff4018bdacf3eef5668
ajchang <ajchang@snorlax.cs.uchicago.edu>
2015-01-28 21:16:34 -0600

    Initial Commit

---
+++ GitHubIssues/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  GitHubIssues
+//
+//  Created by ajchang on 1/28/15.
+//  Copyright (c) 2015 ajchang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
