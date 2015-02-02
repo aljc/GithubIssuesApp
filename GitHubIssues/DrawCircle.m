52a03c1d49e4ba25bf7c8a40f05f50c11ea5bd4f
ajchang <ajchang@omanyte.cs.uchicago.edu>
2015-02-02 16:42:16 -0600

    drew circles, specificed DrawCircle as the custom view class for CircleViewController

--- GitHubIssues/DrawCircle.m
+++ GitHubIssues/DrawCircle.m
@@ -14,12 +14,26 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
+    //get screen dimensions for circle placement
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.width;
+    CGFloat screenHeight = screenRect.size.height;
+    
     // Drawing code
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetLineWidth(context, 4.0);
     CGContextSetStrokeColorWithColor(context,
-                                     [UIColor blueColor].CGColor);
-    CGRect rectangle = CGRectMake(60,170,200,80);
+                                     [UIColor redColor].CGColor);
+
+    CGRect rectangle = CGRectMake(screenWidth/2 - 100.0,screenHeight/2 - 100.0,200,200);
+    CGContextAddEllipseInRect(context, rectangle);
+    CGContextStrokePath(context);
+    
+    context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 4.0);
+    CGContextSetStrokeColorWithColor(context,
+                                     [UIColor greenColor].CGColor);
+    rectangle = CGRectMake(screenWidth/2 - 120.0,screenHeight/2 - 120.0,240,240);
     CGContextAddEllipseInRect(context, rectangle);
     CGContextStrokePath(context);
 }
